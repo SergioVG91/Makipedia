5326f3d776db55a0971c68938dbc8ef231d3e7e9
Sergio Vázquez <sergio.buap.7@hotmail.com>
2019-11-20 18:31:47 -0600

    Inicio de desarrollo del juego secuencial de Maki

---
+++ js/juego_secuencia.js
@@ -0,0 +1,22 @@
+const img_maki_1 = document.getElementById('img_round_maki_ur_1')
+const img_maki_2 = document.getElementById('img_round_maki_ur_2')
+const img_maki_3 = document.getElementById('img_round_maki_ur_3')
+const img_maki_4 = document.getElementById('img_round_maki_ur_4')
+const img_maki_5 = document.getElementById('img_round_maki_ur_5')
+const img_maki_6 = document.getElementById('img_round_maki_ur_6')
+const img_maki_7 = document.getElementById('img_round_maki_ur_7')
+const img_maki_8 = document.getElementById('img_round_maki_ur_8')
+const btn_empezar_juego = document.getElementById('btn_empezar_juego_maki')
+
+class Juego{
+    constructor(){
+
+    }
+
+}
+
+function empezarJuegoMaki(){
+    const juego_maki = new Juego()
+    juego_maki.iniciar()
+}
+
