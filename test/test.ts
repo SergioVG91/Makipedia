2ed34e8bef74e415fe84ec93d616090b549fc05d
Sergio Vázquez <sergio.buap.7@hotmail.com>
2019-11-27 18:14:04 -0600

    Ejercicio patrones terminado

--- test/test.ts
+++ test/test.ts
@@ -5,14 +5,19 @@ interface Observer{
 interface Sujeto{
     suscribir:(observer:Observer) => void
     desuscribir:(observer:Observer) => void
-    notificar:(observer:Observer[]) => void
+ //   notificar:(observer:Observer[]) => void
 }
 
 class BitCoin implements Sujeto{
-    suscriptores: Observer[]
+    suscriptores: Observer[] =[]
+    elemento:HTMLInputElement
 
-    constructor(){
-        this.suscriptores = []
+    constructor(elemento){
+        //this.suscriptores = [] inicializarlo arriba mejor
+        this.elemento = elemento                
+        this.elemento.addEventListener('input', () => {          
+          this.notificar(this.elemento.value);
+        });
     }
 
     suscribir(observer){
@@ -20,24 +25,49 @@ class BitCoin implements Sujeto{
     }
 
     desuscribir(observer){
-        const index = this.suscriptores.indexOf((data:Observer)=>{
-            data==observer
-        })
+        //const index = this.suscriptores.indexOf(observer)
+        const index = this.suscriptores.findIndex(obs => {
+            return obs === observer;
+          });
+        
         this.suscriptores.splice(index,1)
     }
 
-    notificar(observer){
-        
+    notificar(data){        
+        this.suscriptores.forEach((element)=>{
+            element.update(data)
+        })
     }
 }
 
 class Suscriptores implements Observer{
     elemento:HTMLInputElement
-    constructor(){
-        this.elemento = document.querySelector('input2')
+    constructor(elemento){
+        this.elemento = elemento
     }
     update(data){
         this.elemento.value = data
     }
 }
 
+
+const elemento1 = document.getElementById("#input")
+const elemento2 = document.getElementById("#input2")
+const elemento3 = document.getElementById("#input3")
+const elemento4 = document.getElementById("#input4")
+const bit = new BitCoin(elemento1)
+const sus = new Suscriptores(elemento2)
+const sus2 = new Suscriptores(elemento3)
+const sus3 = new Suscriptores(elemento4)
+bit.suscribir(sus)
+bit.suscribir(sus2)
+bit.suscribir(sus3)
+setTimeout(()=>{
+    console.log("desus")
+    bit.desuscribir(sus2)
+},3000)
+
+setTimeout(()=>{
+    console.log("sus")
+    bit.suscribir(sus2)
+},7000)
