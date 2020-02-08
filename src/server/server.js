8787d500757c50d519c509c1bff9d8e82785e7ee
Sergio <sergio.buap.7@hotmail.com>
2020-02-08 14:27:13 -0500

    Fusionando el SSR del curso de passport con el de Makipedia

--- src/server/server.js
+++ src/server/server.js
@@ -4,15 +4,31 @@ import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 import helmet from 'helmet';
+import passport from 'passport';
+import boom from '@hapi/boom';
+import cookieParser from 'cookie-parser';
+import axios from 'axios';
+//import session from 'express-session';
 import main from './routes/main';
 
 dotenv.config();
 
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+
+app.use(express.json()); // body parser
+app.use(cookieParser()); // cookie-parser
+//app.use(session({ secret: config.sessionSecret }));
+
+// para que inicialice la session
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(`${__dirname}/public`));
+
+// Basic Strategy
+require('./utils/auth/strategies/basic');
 
 if (ENV === 'development') {
   console.log('En dev config');
@@ -36,6 +52,50 @@ if (ENV === 'development') {
   app.use(helmet.permittedCrossDomainPolicies());
   app.disable('x-powered-by');
 }
+
+app.post('/auth/sign-in', async (req, res, next) => {
+  passport.authenticate('basic', async (error, data) => {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized('Error la data viene vacía :('));
+      }
+
+      req.login(data, { session: false }, async (error) => {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+
+        res.cookie('token', token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post('/auth/sign-up', async (req, res, next) => {
+  const { body: user } = req;
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: 'post',
+      data: user,
+    });
+
+    res.status(201).json({
+      message: 'User Created',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.get('*', main);
 
